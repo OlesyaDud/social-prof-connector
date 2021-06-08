a8c1ff86baf23b54799d40bfff7b5477abe4d7be
OlesyaDud <wodadevelopment@gmail.com>
2021-06-08 10:52:07 -0400

    initial post user request

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
 
 // Public  / api/users
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Email is required').isEmail(),
+    // password must be at least 5 chars long
+    body('password', 'Password is required').isLength({ min: 5 })
+],
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      res.send();
+  // send data. excess data with req.body 
+});
+
 
 module.exports = router;
