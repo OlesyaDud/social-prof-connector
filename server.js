a8a0cc65425b9a7f2352da686d52be41dfb950b0
OlesyaDud <wodadevelopment@gmail.com>
2021-06-08 10:07:47 -0400

    models, routes added

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const app = express();
 const colors = require('colors');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
 
 app.get('/', (req, res) => res.send('API RUNNING'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
