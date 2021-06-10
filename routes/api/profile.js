c7ab55de07325cedcf32760aa503f6d78902e423
OlesyaDud <wodadevelopment@gmail.com>
2021-06-10 09:07:22 -0400

    github auth added, add/delete experience/education added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../model/User');
 const Profile = require('../../model/Profile');
-const Post = require('../../model/Post');
+const request = require('request');
 const config = require('config');
 const { body, validationResult } = require('express-validator');
 
@@ -142,5 +142,131 @@ router.delete('/', auth, async (req, res) => {
 });
 
 
+// PUT profile experience
+router.put('/experience', [auth, [
+    body('title', 'Title is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {
+        title, company, location, description, from, to, current
+    } = req.body;
+
+    // title---same as title: title
+    const newExp = {
+        title, company, location, description, from, to, current
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.experience.unshift(newExp);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// delete experience
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        // get correct exp to remove
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        // want to take something out
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+
+// PUT profile education
+router.put('/education', [auth, [
+    body('school', 'School is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {
+       school, degree, fieldofstudy, description, from, to, current
+    } = req.body;
+
+    // title---same as title: title
+    const newEdu = {
+        school, degree, fieldofstudy, description, from, to, current
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        profile.education.unshift(newEdu);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// delete education
+router.delete('/education/:edu_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        // get correct exp to remove
+        const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+        // want to take something out
+        profile.education.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
+});
+
+
+// get user repost from GitHub
+router.get('/github/:username', (req, res) => {
+    try {
+       const options = {
+           uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+           method: 'GET',
+           headers: {'user-agent': 'node.js'}
+       };
+
+       request(options, (error, response, body) => {
+        if(error) console.error(error);
+
+        if(response.statusCode !== 200) {
+            return res.status(404).json({msg: 'No github profile found'});
+        }
+
+        // if it is found
+        res.json(JSON.parse(body));
+       });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error'); 
+    }
+})
 
 module.exports = router;
