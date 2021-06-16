3cc6e721711a1e2725aa0970b974b493c33022de
OlesyaDud <wodadevelopment@gmail.com>
2021-06-16 13:27:14 -0400

    changes

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch
+ } from 'react-router-dom';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+const App=()=> {
+  return (
+    <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+        </Switch>
+      </section>     
+    </Fragment>
+    </Router>
+  );
+}
+
+export default App;
